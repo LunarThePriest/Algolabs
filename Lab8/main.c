630c59adcea919042984e40d402d4470a8d05dfb
LunarThePriest <nastya7ka@gmail.com>
2024-03-11 20:03:43 +0200

    lab 8

--- Lab8/main.c
+++ Lab8/main.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int main()
-{
+int main(){
     char txt[30];
+    char initials[10];
+    int j = 2;
 
-    puts("LAB 8. TASK 2: Vyvedennya initsialiv");
+    puts("LAB 8. TASK 2: Vyvedennya initsialiv za dopomohoyu funktsiy biblioteky <string.h>");
     puts("Made by: Koval'chuk Anastasia, KNMS-11");
     puts("\nPlease enter your full name: ");
 
-    gets(txt);
-    for (int i = 0; i < strlen(txt); i++)
-    {
-        if ((int)txt[i] > 64 && (int)txt[i] < 91 || (int)txt[i] > 96 && (int)txt[i] < 123) //letters
-        {
+    fgets(txt, sizeof(txt), stdin);
+
+    for (int i = 0; i < strlen(txt)-1; i++){
+        if (isalpha(txt[i]))
             continue;
-        }
         else{
             switch ((int)txt[i]){
             case 32: // space
+                initials[j] = txt[i + 1];
+                initials[j + 1] = '.';
+                j += 2;
+                continue;
             case 39: //"hyphen(defis)"
             case 45: //"apostrophe"
                 continue;
             default:
-                puts("Input error. Please try again");
+                printf("Input error at character %c (number %d). Please try again", txt[i], i);
                 return (1);
             }
         }
     }
 
-    printf("%s", txt);
+    initials[0] = txt[0];
+    initials[1] = '.';
+
+    printf("Initials: %s\n", initials);
 }
